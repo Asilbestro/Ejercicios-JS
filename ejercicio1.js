38b4f908c839cfee23890786d8eefa376e1b9792
Asilbestro <asilbestro7@gmail.com>
2023-05-10 15:20:40 -0300

    Se resolvieron los ejercicios 1 y 2

---
+++ ejercicio1.js
@@ -0,0 +1,25 @@
+// Tienes un array de números, tu reto es retornar los números de ese array multiplicados por dos.
+
+// Para solucionarlo vas a encontrar una función llamada multiplyElements que recibe un parámetro de entrada:
+
+// array: Un array de números
+// Dentro del cuerpo de la función multiplyElements debes escribir tu solución.
+
+//     Ejemplo 1:
+
+// Input: [2, 4, 5, 6, 8]
+// Output: [4, 8, 10, 12, 16]
+
+// Ejemplo 2:
+
+// Input: [1, 1, -2, -3]
+// Output: [1, 1, -4, -6]
+
+function multiplyElements(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = array[i] * 2;
+    }
+    return array;
+}
+
+console.log(multiplyElements([2,3,6,4,-2]));
---
+++ ejercico2.js
@@ -0,0 +1,64 @@
+// Tienes un array de objetos que representan datos de personas con los siguientes atributos:
+
+// name: string
+// lastName: string
+// age: number
+// Tu reto es retornar un array de strings con solo los nombres, para solucionarlo vas a encontrar una función llamada llamada getNames que recibe un parámetro de entrada:
+
+// array: Un array de objetos.
+// Dentro del cuerpo de la función getNames debes escribir tu solución.
+
+//     Ejemplo:
+
+// Input:
+// [
+//     {
+//         name: 'Nicolas',
+//         lastName: 'Molina',
+//         age: 28
+//     },
+//     {
+//         name: 'Valentina',
+//         lastName: 'Molina',
+//         age: 19
+//     },
+//     {
+//         name: 'Zulema',
+//         lastName: 'Vicente',
+//         age: 21
+//     },
+// ]
+
+// Output:
+// ['Nicolas', 'Valentina', 'Zulema']
+
+
+function getNames(array) {
+    const namesPerson = [];
+    for (let i = 0; i < array.length; i++) {
+        namesPerson.push(array[i].name);
+    }
+    return namesPerson;
+}
+
+console.log(getNames([
+    {
+        name: 'Nicolas',
+        lastName: 'Molina',
+        age: 28
+    },
+    {
+        name: 'Valentina',
+        lastName: 'Molina',
+        age: 19
+    },
+    {
+        name: 'Zulema',
+        lastName: 'Vicente',
+        age: 21
+    },{
+    name: 'Agustín',
+    lastName: 'Silbestro',
+    age: 21
+    }
+]));
