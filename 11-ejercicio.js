bb19727387e180b7e23507bb7334750d7e5bf733
Asilbestro <asilbestro7@gmail.com>
2023-05-12 08:30:12 -0300

    Se resolvió el ejercicio 11

---
+++ 11-ejercicio.js
@@ -0,0 +1,46 @@
+/*
+Tu reto es retornar true si dentro de array de números todos son pares, para solucionarlo vas a 
+encontrar una función llamada checkArray que recibe un parámetro de entrada:
+
+numbers: Un array de números
+Dentro del cuerpo de la función checkArray debes escribir tu solución.
+
+Ejemplo 1:
+
+Input: [2, 4, 6, 8, 10]
+Output: true
+
+Ejemplo 2:
+
+Input: [1, 3, 5, 7, 10, 11]
+Output: false
+
+Ejemplo 3:
+
+Input: [1, 3, 5]
+Output: false
+
+Ejemplo 4:
+
+Input: []
+Output: false
+
+
+*/
+
+function checkArray(array) {
+    let isPar = 0;
+    for (let i = 0; i < array.length; i++) {
+        if ((array[i] % 2) === 0) {
+            isPar++;
+        }
+    }
+    if (isPar == array.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Probando la solución del ejercicio
+console.log(checkArray([2, 4, 6, 8]));
